de58df3a9650ebe63336656187b20e19eb9eaf0a
Michael Yu <ItzWarty@gmail.com>
2014-11-19 19:57:35 -0800

    Workaround - forward slashes in assembly info aren't properly handled by jenkins.sh.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Dargon Portable Object Library")]
-[assembly: AssemblyDescription("Portable Object serialization/deserialization.")]
+[assembly: AssemblyDescription("Portable Object serialization and deserialization.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("The Dargon Project")]
 [assembly: AssemblyProduct("libdpo")]
